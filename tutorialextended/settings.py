edc5f3225420a63e5643ec980706291610bcc485
Farhan <alamfarhan389@gmail.com>
2024-03-21 22:37:39 +0530

    added comment

--- tutorialextended/settings.py
+++ tutorialextended/settings.py
@@ -33,7 +33,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'host_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = 'password' # read from env variable
 
 # Application definition
 
