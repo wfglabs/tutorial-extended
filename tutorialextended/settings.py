9d82e23211ac38b2ec938331a613a8756956b48c
Farhan <alamfarhan389@gmail.com>
2024-03-21 22:35:03 +0530

    removed credentials

--- tutorialextended/settings.py
+++ tutorialextended/settings.py
@@ -32,7 +32,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'wfglabs@gmail.com'
+EMAIL_HOST_USER = 'host_email@gmail.com'
 EMAIL_HOST_PASSWORD = 'ciligewbopchhhrl'
 
 # Application definition
